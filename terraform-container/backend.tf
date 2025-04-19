767bc9ae3d730356dc7fa207d32930f014d2a6a9
aaron lim <aaron.limse@hotmail.com>
2025-04-19 12:31:50 +0800

    modified: terraform-container/backend.tf terraform-container/ecs.tf

--- terraform-container/backend.tf
+++ terraform-container/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "sctp-ce9-tfstate"
-    key    = "lach/proj-aws-ecs-cicd-container.tfstate"
+    key    = "lach/proj-aws-ecs-cicd-container.tfstate" # update name
     region = "us-east-1"
   }
 }
